efc7345e7c8523456ec4b45698648b392cae949b
Nikita Rudenko <n.rud3nko@gmail.com>
2019-09-04 16:54:25 +0300

    build: initial project setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+// Components
 import App from './App';
+// Service worker
 import * as serviceWorker from './serviceWorker';
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
