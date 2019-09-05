09f07aef63e1c0ddc4e7c091f696a6a29cb684fe
Nikita Rudenko <n.rud3nko@gmail.com>
2019-09-05 16:20:55 +0300

    chore: create theme

---
+++ src/config/theme/index.js
@@ -0,0 +1,40 @@
+export const theme = {
+    colors: {
+        white: 'rgb(255, 255, 255)',
+        grey: {
+            light: 'rgb(226, 225, 224)',
+            dark: 'rgb(102, 102, 102)'
+        },
+        blue: {
+            light: 'rgb(218, 232, 252)',
+            dark: 'rgb(108, 142, 191)'
+        },
+        green: {
+            light: 'rgb(213, 232, 212)',
+            dark: 'rgb(130, 179, 102)'
+        },
+        orange: {
+            light: 'rgb(255, 230, 204)',
+            dark: 'rgb(215, 155, 0)'
+        },
+        yellow: {
+            light: 'rgb(255, 242, 204)',
+            dark: 'rgb(214, 182, 86)'
+        },
+        red: {
+            light: 'rgb(248, 206, 204)',
+            dark: 'rgb(184, 84, 80)'
+        },
+        violet: {
+            light: 'rgb(225, 213, 231)',
+            dark: 'rgb(150, 115, 166)'
+        }
+    },
+    boxShadow: {
+        lightest: '0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.14)',
+        lighter: '0 3px 6px rgba(0, 0, 0, 0.14), 0 3px 6px rgba(0, 0, 0, 0.16)',
+        normal: '0 8px 8px rgba(0, 0, 0, 0.16), 0 4px 6px rgba(0, 0, 0, 0.18)',
+        stronger: '0 10px 20px rgba(0, 0, 0, 0.18), 0 6px 6px rgba(0, 0, 0, 0.22)',
+        strongest: '0 19px 38px rgba(0, 0, 0, 0.30), 0 15px 12px rgba(0, 0, 0, 0.24)'
+    } 
+};
